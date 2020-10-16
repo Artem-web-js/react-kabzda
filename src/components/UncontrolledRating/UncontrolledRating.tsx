29464a3b76aca64f1af345874584babe795eea33
Artem-web-js <68924210+Artem-web-js@users.noreply.github.com>
2020-10-17 01:17:02 +0300

    callbacks 1

--- src/components/UncontrolledRating/UncontrolledRating.tsx
+++ src/components/UncontrolledRating/UncontrolledRating.tsx
@@ -2,6 +2,8 @@ import React, {useState} from "react";
 
 type StarType = {
     selected: boolean
+    value: 1 | 2 | 3 | 4 | 5
+    setValue: (value: 1 | 2 | 3 | 4 | 5) => void
 }
 
 type RatingType = {
@@ -12,13 +14,13 @@ export function UncontrolledRating(props: RatingType) {
     let [value, setValue] = useState(0);
     return (
         <div>
-            <Star selected={value > 0}/><button onClick={() => {setValue(1)}}>1</button>
-            <Star selected={value > 1}/><button onClick={() => {setValue(2)}}>2</button>
-            <Star selected={value > 2}/><button onClick={() => {setValue(3)}}>3</button>
-            <Star selected={value > 3}/><button onClick={() => {setValue(4)}}>4</button>
-            <Star selected={value > 4}/><button onClick={() => {setValue(5)}}>5</button>
+            <Star selected={value > 0} setValue={setValue} value={1}/>
+            <Star selected={value > 1} setValue={setValue} value={2}/>
+            <Star selected={value > 2} setValue={setValue} value={3}/>
+            <Star selected={value > 3} setValue={setValue} value={4}/>
+            <Star selected={value > 4} setValue={setValue} value={5}/>
         </div>
     )
 }
 
-const Star = (props: StarType) => props.selected ? <span><b>star </b></span> : <span>star </span>
+const Star = (props: StarType) => <span onClick={() => {props.setValue(props.value)}}>{props.selected ? <b>star </b> : "star "}</span>
