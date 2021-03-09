0d5b0d44d65beb2a9d3dce41960dcdc03922a5f2
Artem-web-js <zenticul@gmail.com>
2021-03-09 21:11:27 +0200

    useEffect - clock

---
+++ src/components/useEffect/clock.stories.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {Clock} from "./clock";
+
+export default {
+    title: 'Clock',
+    component: Clock
+}
+
+export const BAseExample = () => {
+    return <Clock/>
+}
